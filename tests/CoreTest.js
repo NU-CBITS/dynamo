82c57efb4cada427053d675e8722874246718e28
Eric Carty-Fickes <ericcf@northwestern.edu>
2013-05-07 10:03:51 -0500

    extended Core tests; added some Core.Models tests

--- tests/CoreTest.js
+++ tests/CoreTest.js
@@ -1,4 +1,4 @@
-var assert = chai.assert;
+var USERS;
 
 describe("Core", function() {
   function TestUser() {
@@ -18,12 +18,12 @@ describe("Core", function() {
         localStorage.clear();
         Dynamo._CurrentUser = null;
       })
-      
+
       it("should return a preassigned current user", function() {
         Dynamo._CurrentUser = user;
         assert.equal(user, Dynamo.CurrentUser());
       })
-      
+
       it("should return a predefined current user", function() {
         Dynamo.CURRENT_USER_ID = user.get("guid");
         Dynamo.CURRENT_GROUP_ID = user.get("group_id");
@@ -31,76 +31,76 @@ describe("Core", function() {
         assert.equal(user.get("group_id"), Dynamo.CurrentUser().get("group_id"));
         Dynamo.CURRENT_USER_ID = Dynamo.CURRENT_GROUP_ID = null
       })
-      
-      // this breaks all following tests until USERS is namespaced
-      /*describe("when there is a stored user", function() {
-        before(function() {
-          localStorage.setItem("CurrentUser", user);
+
+      describe("when there is a stored user", function() {
+        describe("and there is not a user collection", function() {
+          before(function() {
+            localStorage.setItem("CurrentUser", JSON.stringify(user.toJSON()));
+          })
+
+          it("should return the reified stored serialized user", function() {
+            assert.equal(user.guid, Dynamo.CurrentUser().guid);
+          })
         })
-        
+
         describe("and there is a user collection", function() {
           var otherUser = TestUser();
-          
+
           before(function() {
+            localStorage.setItem("CurrentUser", JSON.stringify(user.toJSON()));
             otherUser.set("guid", user.get("guid"));
             USERS = new Dynamo.UserCollection([otherUser]);
           })
-          
+
           it("should return the user from the collection", function() {
-            assert.equal(otherUser, Dynamo.CurrentUser());
+            assert.equal(otherUser.guid, Dynamo.CurrentUser().guid);
           })
         })
-        
-        describe("and there is not a user collection", function() {
-          it("should return the stored serialized user", function() {
-            assert.equal(user, Dynamo.CurrentUser());
-          })
-        })
-      })*/
-      
+      })
+
       describe("when a login is required", function() {
         before(function() {
           Dynamo.requireLogin();
           sinon.stub(Dynamo, "redirectTo");
         })
-        
+
         after(function() {
           Dynamo.redirectTo.restore();
           Dynamo._loginRequired = false;
         })
-        
+
         it("should redirect to the login page", function() {
           Dynamo.CurrentUser();
           assert.equal("login.html", Dynamo.redirectTo.getCall(0).args[0]);
         })
       })
-      
+
       describe("when no login is required", function() {
         before(function() {
           this.server = sinon.fakeServer.create();
           this.server.respondWith("POST", /\/users/,
                                   [201, { "Content-Type": "application/json" }, '']);
         })
-        
+
         beforeEach(function() {
           Dynamo.CurrentUser();
           this.server.respond();
         })
-        
+
         after(function() {
           this.server.restore();
         })
-        
+
         it("should construct a new user", function() {
           assert.isDefined(Dynamo._CurrentUser);
           assert.isNotNull(Dynamo._CurrentUser);
         })
-        
+
         it("should save the new user in the store", function() {
           assert.isDefined(localStorage.getItem("CurrentUser"));
           assert.isNotNull(localStorage.getItem("CurrentUser"));
         })
-        
+
         it("should save the new user to the server", function() {
           assert.isDefined(this.server.requests[0]);
           assert.isNotNull(this.server.requests[0]);
@@ -129,21 +129,21 @@ describe("Core", function() {
       })
     })
   })
-  
+
   describe(".addQueryVarToUrl", function() {
     it("should not add a duplicate parameter", function() {
       assert.equal("foo?a=b", addQueryVarToUrl("a", "b", "foo?a=b"));
     })
-    
+
     it("should add a distinct parameter", function() {
-      assert.equal("foo?abc=def", addQueryVarToUrl("a", "b", "foo?abc=def&a=b"));
+      assert.equal("foo?abc=def&a=b", addQueryVarToUrl("a", "b", "foo?abc=def&a=b"));
     })
-    
+
     it("should add a parameter that matches a substring of the URL", function() {
       assert.equal("foo?fo=bar", addQueryVarToUrl("fo", "bar", "foo"));
     })
   })
-  
+
   describe(".addSessionVarsToUrl", function() {
       before(function() {
         sinon.stub(Dynamo, "CurrentUser", function() { return user });
