8ac11ed2eadcbe7a72c6a283b7371a84ffd710d4
Eric Carty-Fickes <ericcf@northwestern.edu>
2013-05-09 10:09:22 -0500

    added Dynamo.ShowArrayView tests

--- tests/Core.ViewsTest.js
+++ tests/Core.ViewsTest.js
@@ -1,28 +1,63 @@
-describe("Dynamo.ShowXelementSimpleView", function() {
-  var attrs = window.dynamoTestFixtures.UnitaryXelementAttributes();
-  var vals = attrs.xel_data_values;
-
-  before(function() {
+describe("Core.Views", function() {
+  beforeEach(function() {
     $("body").append("<div id='sandbox'>");
   })
 
-  after(function() {
+  afterEach(function() {
     $("body > #sandbox").remove();
   })
 
-  it("should render only the title and specified attrs", function() {
-    var model = new Dynamo.UnitaryXelement(attrs);
-    var view = new Dynamo.ShowXelementSimpleView({
-      model: model,
-      el: "#sandbox",
-      atts_to_display: ["cats", "recipe", "hammerTime", "quotation"]
-    });
-    view.render();
-    assert.equal(vals.title, $.trim($(".attribute.title").html()));
-    assert.equal(JSON.stringify(vals.cats), $(".attribute.cats").html());
-    assert.equal(JSON.stringify(vals.recipe), $(".attribute.recipe").html());
-    assert.equal(JSON.stringify(new Date(vals.hammerTime)), $(".attribute.hammerTime").html());
-    assert.equal(JSON.stringify(vals.quotation), $(".attribute.quotation").html());
-    assert.equal(0, $(".attribute.onceUponATime").length);
+  /*describe("Dynamo.ShowXelementSimpleView", function() {
+    var attrs = window.dynamoTestFixtures.UnitaryXelementAttributes();
+    var vals = attrs.xel_data_values;
+
+    it("should render only the title and specified attrs", function() {
+      var model = new Dynamo.UnitaryXelement(attrs);
+      var view = new Dynamo.ShowXelementSimpleView({
+        model: model,
+        el: "#sandbox",
+        atts_to_display: ["cats", "recipe", "hammerTime", "quotation"]
+      });
+      view.render();
+      assert.equal(vals.title, $.trim($(".attribute.title").html()));
+      assert.equal(JSON.stringify(vals.cats), $(".attribute.cats").html());
+      assert.equal(JSON.stringify(vals.recipe), $(".attribute.recipe").html());
+      assert.equal(JSON.stringify(new Date(vals.hammerTime)), $(".attribute.hammerTime").html());
+      assert.equal(JSON.stringify(vals.quotation), $(".attribute.quotation").html());
+      assert.equal(0, $(".attribute.onceUponATime").length);
+    })
+  })*/
+
+  describe("Dynamo.ShowArrayView", function() {
+    var view;
+
+    beforeEach(function() {
+      $("#sandbox").append("<div class='array-view'></div>");
+      view = new Dynamo.ShowArrayView({
+        container: "#sandbox",
+        elementTemplate: "<div class='item'>(%= item %)</div>",
+        contentWhenEmpty: "The dark side"
+      });
+    })
+
+    it("should render each item", function() {
+      view.getArrayFn = function() { return ["Han Solo"] };
+      view.render();
+      assert.equal("Han Solo", $(".array-view:first").text());
+    })
+
+    it("should render the default when the array is empty", function() {
+      view.getArrayFn = function() { return [] };
+      view.render();
+      assert.equal("The dark side", $(".array-view:first").text());
+    })
+
+    it("should add the onElementClick listener to each el with class item", function(done) {
+      // wrapping done in anon fn b/c it doesn't like being called w/ non-err args
+      view.onElementClick = function() { done() };
+      view.getArrayFn = function() { return ["Han Solo"] };
+      view.render();
+      $(".array-view > .item").trigger("click");
+    })
   })
 })
